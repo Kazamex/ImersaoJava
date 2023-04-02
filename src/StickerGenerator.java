18862828b5d4c647561daff5c29b4ea298e12859
Tadao Kazama <robertotadaokazama@gmail.com>
2023-04-02 20:41:13 -0300

    Insert outline on the text sticker

--- src/StickerGenerator.java
+++ src/StickerGenerator.java
@@ -1,6 +1,11 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
@@ -10,16 +15,12 @@ import javax.imageio.ImageIO;
 public class StickerGenerator {
     public void generate(InputStream inputStream, String fileName, String directory, String itemName) throws Exception{
         //Read image
-        //InputStream inputStream = new FileInputStream(new File("entrada/imagem.jpg"));
-        //InputStream inputStream = new URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg").openStream();
         BufferedImage originalImage = ImageIO.read(inputStream);
-
-        int fontSize = 64;
-        int extraHeight = 200;
-
+        
         // Create new image in memory with transparency and new size
         int width = originalImage.getWidth();
         int height = originalImage.getHeight();
+        int extraHeight = 200;
         int newHeight = height + extraHeight;
         BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
@@ -28,13 +29,28 @@ public class StickerGenerator {
         graphics.drawImage(originalImage, 0, 0,null);
         
         // Font config
+        int fontSize = 64;
         var font = new Font("Impact", Font.BOLD, fontSize);
         graphics.setColor(Color.CYAN);
         graphics.setFont(font);
 
         // Writes new line on the new image 
-        int subractHeight = (extraHeight / 2) - (fontSize / 2); 
-        graphics.drawString(itemName, (width/2) - ((fontSize * itemName.length())/4 )  , newHeight - subractHeight);
+        //Calculate position of the text
+        int positionX = (width/2) - ((fontSize * itemName.length())/4);
+        int positionY = newHeight - ((extraHeight / 2) - (fontSize / 2));
+        graphics.drawString(itemName, positionX  , positionY);
+
+        //Creating a outline
+        FontRenderContext fontRender = graphics.getFontRenderContext();
+        TextLayout text = new TextLayout(itemName,font, fontRender);
+        Shape shape = text.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(positionX, positionY);
+        graphics.transform(transform);
+        graphics.setColor(Color.BLACK);
+        graphics.setStroke(new BasicStroke(5f));
+        graphics.draw(shape);
+        graphics.setClip(shape);
 
         // Write the image to a new file
         ImageIO.write(newImage, "png", new File(directory + fileName));
